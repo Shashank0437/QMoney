9cc692f1b7696131c222c47e6290fb4af3faeecc
Shashank0437 <54523830+Shashank0437@users.noreply.github.com>
2022-07-20 23:28:18 +0530

    Delete tingo_curl.sh

--- tingo_curl.sh
+++
@@ -1,3 +0,0 @@
-curl https://api.tiingo.com/tiingo/daily/AAPL/prices?token=f138a3010fe1981e894e06019ed1062f51750f61
-
-curl https://api.tiingo.com/tiingo/daily/GOOGL/prices?token=f138a3010fe1981e894e06019ed1062f51750f61
